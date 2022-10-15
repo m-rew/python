27c6ab68965f8480d643ab770e8304fbbc651d1c
Andrew <m_rew@vk.com>
2022-10-15 13:22:53 +0300

    Изменения в дз семинар 2

--- seminar2/dop-task1/task4.py
+++ seminar2/dop-task1/task4.py
@@ -2,6 +2,23 @@
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
+# from random import *
+# n = int(input())
+# some_list = []
+# for _ in range(n):
+#     some_list.append(randint(-n, n))
+# print(some_list)
+# with open('file.txt', 'w', encoding='utf-8') as f:
+#     for _ in range(randint(1, n)):
+#         f.write(str(randint(0, n - 1)) + '\n')
+# fact = 1
+# with open('file.txt', 'r', encoding='utf-8') as f:
+#     f = f.read().splitlines()
+#     for number in f:
+#         fact *= some_list[int(number)]
+# print(fact)
+
+
 file = open('file.txt', 'r')
 file_list = [line.strip() for line in file]
 file_list = list(map(int, file_list))
