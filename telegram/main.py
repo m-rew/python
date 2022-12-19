16fe39e70f3cced65ce0c01d8a81fdcfe692a661
Andrew <m_rew@vk.com>
2022-12-19 20:53:15 +0300

    Bot

--- telegram/main.py
+++
@@ -1,9 +0,0 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
-import bot_answer
-
-app = ApplicationBuilder().token("5959586307:AAHg6Jpsqc4qEv4eTgsSn4SXjvI8EO482QY").build()
-
-app.add_handler(CommandHandler("start", bot_answer.start_callback))
-app.add_handler(CommandHandler("weather", bot_answer.weather_callback))
-
-app.run_polling()
--- telegram/weather.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def get_weather(city):
-    url = 'http://api.weatherapi.com/v1/current.json'
-    params = {'key': '6580ce5f3a1b414d80b134352221912', 'q': {city}, 'aqi': 'no'}
-
-    return requests.get(url, params).json().get('current').get('temp_c')
